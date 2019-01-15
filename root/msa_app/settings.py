a302ffa74d489bc006e75cd3daf22d94ef41cb2d
toshiki <toshiki1007@gmail.com>
2019-01-15 06:45:13 +0000

    alter　settings

--- root/msa_app/settings.py
+++ root/msa_app/settings.py
@@ -124,7 +124,8 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-#STATIC_URL = '/static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
 
 XRAY_RECORDER = {
